e127f9183b4070ab015aa70a448baaf00b9d182e
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-10-18 17:18:34 +0200

    Revert "Moved location determination to clock.js"
    This reverts commit df2df9055c0f8ece70860d21d9ef0faf02e14882.

--- src/clock.js
+++ src/clock.js
@@ -7,9 +7,6 @@ import i18next from "i18next";
 const Clock = (props) => {
     const [date, setDate] = useState(new Date());
     const [timezone, setTimezone] = useState(props.timezone);
-    const [city, setCity] = useState(props.city);
-    const [country, setCountry] = useState(props.country);
-
     let timerID = 0;
 
     useEffect(() => {
@@ -19,57 +16,10 @@ const Clock = (props) => {
             }, 1000 - new Date().getMilliseconds());
         }
 
-        new Promise((resolve, reject) => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    resolve(cityMapping.map(item => ({
-                        city: item.city,
-                        country: item.country,
-                        timezone: item.timezone,
-                        distance: getDistance(position.coords.latitude, position.coords.longitude, item.lat, item.lng)
-                    })).sort((a, b) => a.distance - b.distance)[0]);
-                }, () => reject());
-            } else {
-                reject();
-            }
-        }).then(cityInfo => {
-            setTimeout(() => {
-                setCity(cityInfo.city);
-                setCountry(cityInfo.country);
-                setTimezone(cityInfo.timezone);
-                props.setDefaults(cityInfo.city, cityInfo.country, cityInfo.timezone);
-            });
-
-        }, () => {});
-
         return () => {
             clearInterval(timerID);
         };
     }, []);
-
-    // https://www.geeksforgeeks.org/program-distance-two-points-earth/
-    function getDistance(lat1, lon1, lat2, lon2) {
-
-        // The math module contains a function
-        // named toRadians which converts from
-        // degrees to radians.
-        lon1 = lon1 * Math.PI / 180;
-        lon2 = lon2 * Math.PI / 180;
-        lat1 = lat1 * Math.PI / 180;
-        lat2 = lat2 * Math.PI / 180;
-
-        // Haversine formula
-        let dlon = lon2 - lon1;
-        let dlat = lat2 - lat1;
-        let a = Math.pow(Math.sin(dlat / 2), 2)
-            + Math.cos(lat1) * Math.cos(lat2)
-            * Math.pow(Math.sin(dlon / 2), 2);
-
-        let c = 2 * Math.asin(Math.sqrt(a));
-
-        let r = 6371; // Radius of earth in kilometers. Use 3956 for miles
-        return (c * r);
-    }
 
     function getItems(input) {
         const inputUpper = input.toLocaleUpperCase();
@@ -86,7 +36,8 @@ const Clock = (props) => {
         <div className="clock">
             <div className="time-zone-name">
                 <AutocompleteDropdown
-                    text={city + ', ' + country}
+                    text={props.city + ', ' + props.country}
+                    disabled={props.disabled}
                     getItems={(input) => getItems(input)} 
                     onChange={(value) => setTimezone(value)} />
             </div>
