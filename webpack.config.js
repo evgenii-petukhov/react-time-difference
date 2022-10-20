a2a2c7412bd98afbbc6c87db99efea61f3148153
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-10-20 12:06:09 +0200

    Added a favicon for various devices

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,12 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "src/favicon.png", to: "favicon.png" },
+                { from: "src/favicon/clock-180px.png", to: "apple-touch-icon.png" },
+                { from: "src/favicon/clock-192px.png", to: "icon-192px.png" },
+                { from: "src/favicon/clock-512px.png", to: "icon-512px.png" },
+                { from: "src/favicon/clock.ico", to: "favicon.ico" },
+                { from: "src/favicon/clock.svg", to: "icon.svg" },
+                { from: "src/favicon/manifest.webmanifest", to: "manifest.webmanifest" },
             ],
         }),
     ],
