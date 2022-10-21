46c03c9675686778a1a0993bd0a5465b7cd7782c
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-10-21 15:17:54 +0200

    Fixed location determination on start-up on iOS. Added map files.

--- src/clock-app.js
+++ src/clock-app.js
@@ -8,12 +8,12 @@ export default class ClockApp {
         const root = ReactDOM.createRoot(domContainer);
 
         const defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        const cityInfo = defaultTimezone.includes('UTC') ? ({
+        const cityInfo = defaultTimezone.includes('UTC') || !defaultTimezone.includes('/') ? ({
             city: 'New York',
             country: 'United States of America',
             timezone: 'America/New_York'
         }) : lookupViaCity(defaultTimezone.split('/')[1])[0];
 
-        root.render(<ClockCollection defaultTimezone={defaultTimezone} defaultCity={cityInfo.city} defaultCountry={cityInfo.country} />);
+        root.render(<ClockCollection defaultTimezone={cityInfo.timezone} defaultCity={cityInfo.city} defaultCountry={cityInfo.country} />);
     }
 }
