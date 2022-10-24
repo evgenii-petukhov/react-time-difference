c28231116d8d79a1d3b097ff47f475bab994c158
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-10-24 23:42:12 +0200

    Fixed image shaking: it shook even on adding a new clock

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,37 +3,35 @@ export { Carousel };
 const Carousel = (props) => {
     const carouselId = `clock-image-carousel-${props.clockId}`;
 
-    return props.images && <div className={props.isChangedManually ? '' : 'default-images'}>
-        <div id={carouselId} className="carousel slide" data-bs-ride="true">
-            <div className="carousel-inner">
-                {
-                    props.images.map((image, index) => <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
-                        <div className="location-image"
-                            style={{ background: `url('${image}') center center no-repeat` }}></div>
-                    </div>)
-                }
-            </div>
-            <div className="carousel-indicators">
-                {
-                    props.images.map((_, index) => <button type="button"
-                        data-bs-target={`#${carouselId}`}
-                        data-bs-slide-to={index}
-                        className={index === 0 ? 'active' : ''}
-                        key={index}></button>)
-                }
-            </div>
+    return props.images && <div id={carouselId} className={`carousel slide ${props.isChangedManually ? '' : 'default-images'}`} data-bs-ride="true">
+        <div className="carousel-inner">
             {
-                props.images.length > 1 && <button className="carousel-control-prev" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-            }
-            {
-                props.images.length > 1 && <button className="carousel-control-next" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="next">
-                    <span className="carousel-control-next-icon"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+                props.images.map((image, index) => <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                    <div className="location-image"
+                        style={{ background: `url('${image}') center center no-repeat` }}></div>
+                </div>)
             }
         </div>
+        <div className="carousel-indicators">
+            {
+                props.images.map((_, index) => <button type="button"
+                    data-bs-target={`#${carouselId}`}
+                    data-bs-slide-to={index}
+                    className={index === 0 ? 'active' : ''}
+                    key={index}></button>)
+            }
+        </div>
+        {
+            props.images.length > 1 && <button className="carousel-control-prev" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="prev">
+                <span className="carousel-control-prev-icon"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+        }
+        {
+            props.images.length > 1 && <button className="carousel-control-next" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="next">
+                <span className="carousel-control-next-icon"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        }
     </div>;
 }
