a43da3a60fc521a1eae02d951751ea45cf8e3731
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-10-24 23:09:18 +0200

    Customized the carousel: added indicators and arrows

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,23 +3,33 @@ export { Carousel };
 const Carousel = (props) => {
     const carouselId = `clock-image-carousel-${props.clockId}`;
 
-    return props.images && <div id={carouselId} className={`carousel slide ${props.isChangedManually ? '' : 'default-images'}`} data-bs-ride="carousel">
-        <div className="carousel-indicators">
-            {
-                props.images.map((_, index) => <button type="button"
-                    data-bs-target={`#${carouselId}`}
-                    data-bs-slide-to={index}
-                    className={index === 0 ? 'active' : ''}
-                    key={index}></button>)
-            }
-        </div>
-        <div className="carousel-inner">
-            {
-                props.images.map((image, index) => <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
-                    <div className="location-image"
+    return props.images && <div className={props.isChangedManually ? '' : 'default-images'}>
+        <div id={carouselId} className="carousel slide" data-bs-ride="true">
+            <div className="carousel-inner">
+                {
+                    props.images.map((image, index) => <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                        <div className="location-image"
                     style={{background: `url('${image}') center center no-repeat`}}></div>
-                </div>)
-            }
+                    </div>)
+                }
+            </div>
+            <div className="carousel-indicators">
+                {
+                    props.images.map((_, index) => <button type="button"
+                        data-bs-target={`#${carouselId}`}
+                        data-bs-slide-to={index}
+                        className={index === 0 ? 'active' : ''}
+                        key={index}></button>)
+                }
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="prev">
+                <span className="carousel-control-prev-icon"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target={`#${carouselId}`} data-bs-slide="next">
+                <span className="carousel-control-next-icon"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
     </div>;
 }
