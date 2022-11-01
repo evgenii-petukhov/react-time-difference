ef2d72141ffc7195807a3409f0d2156eace3edf2
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-11-01 21:31:51 +0100

    Added assertions to the Carousel component tests

--- tests/carousel.test.js
+++ tests/carousel.test.js
@@ -17,7 +17,9 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    root.unmount();
+    act(() => {
+        root.unmount();
+    });
     container.remove();
     container = null;
 });
@@ -27,30 +29,54 @@ describe('doesn\'t render anything', () => {
         act(() => {
             root.render(<Carousel />);
         });
+
         const carousel = container.querySelector('.carousel');
         expect(carousel).toBeNull();
-        const images = container.querySelectorAll('.location-image');
-        expect(images).toHaveLength(0);
+
+        const items = container.querySelectorAll('.carousel-item');
+        expect(items).toHaveLength(0);
+
+        const arrowLeft = container.querySelector('.carousel-control-prev');
+        expect(arrowLeft).toBeNull();
+
+        const arrowRight = container.querySelector('.carousel-control-next');
+        expect(arrowRight).toBeNull();
     });
 
     it('if `images` is null', () => {
         act(() => {
             root.render(<Carousel images={null} />);
         });
+
         const carousel = container.querySelector('.carousel');
         expect(carousel).toBeNull();
-        const images = container.querySelectorAll('.location-image');
-        expect(images).toHaveLength(0);
+
+        const items = container.querySelectorAll('.carousel-item');
+        expect(items).toHaveLength(0);
+
+        const arrowLeft = container.querySelector('.carousel-control-prev');
+        expect(arrowLeft).toBeNull();
+
+        const arrowRight = container.querySelector('.carousel-control-next');
+        expect(arrowRight).toBeNull();
     });
 
     it('if `images` is an empty array', () => {
         act(() => {
             root.render(<Carousel images={[]} />);
         });
+
         const carousel = container.querySelector('.carousel');
         expect(carousel).toBeNull();
-        const images = container.querySelectorAll('.location-image');
-        expect(images).toHaveLength(0);
+
+        const items = container.querySelectorAll('.carousel-item');
+        expect(items).toHaveLength(0);
+
+        const arrowLeft = container.querySelector('.carousel-control-prev');
+        expect(arrowLeft).toBeNull();
+
+        const arrowRight = container.querySelector('.carousel-control-next');
+        expect(arrowRight).toBeNull();
     });
 });
 
@@ -59,9 +85,20 @@ it('renders images', () => {
     act(() => {
         root.render(<Carousel clockId='1' images={blobs} isShakeAnimationRequired='true' />);
     });
+
     const carousel = container.querySelector('.carousel');
     expect(carousel).not.toBeNull();
+
+    const items = container.querySelectorAll('.carousel-item');
+    expect(items).toHaveLength(3);
+
     const images = container.querySelectorAll('.location-image');
     expect(images).toHaveLength(3);
     expect(Array.from(images).map(i => i.style.background)).toEqual(blobs.map(b => `url(${b}) no-repeat center`));
+
+    const arrowLeft = container.querySelector('.carousel-control-prev');
+    expect(arrowLeft).not.toBeNull();
+
+    const arrowRight = container.querySelector('.carousel-control-next');
+    expect(arrowRight).not.toBeNull();
 });
