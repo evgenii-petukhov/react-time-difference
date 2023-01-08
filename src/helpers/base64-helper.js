9777befa4f4e6396615cce3059c10c4fb3e3176b
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-01-08 21:50:01 +0100

    Added a test for the downloadAndEncodeToBase64 helper

--- src/helpers/base64-helper.js
+++ src/helpers/base64-helper.js
@@ -1,7 +1,7 @@
 function downloadAndEncodeToBase64(url) {
     return new Promise((resolve, reject) => {
         fetch(url)
-            .then(response => response.ok ? response.blob() : null)
+            .then(response => response.ok ? (typeof response.blob === 'function' ? response.blob() : null) : null)
             .then(blob => {
                 if (blob && blob.type === 'image/jpeg') {
                     const reader = new FileReader();
--- tests/base64-helper.test.js
+++ tests/base64-helper.test.js
@@ -1,46 +1,93 @@
 import downloadAndEncodeToBase64 from '../src/helpers/base64-helper';
 
 describe('downloadAndEncodeToBase64', () => {
-    it('image/jpeg', () => {
-        // Arrange
-        // https://www.leighhalliday.com/mock-fetch-jest
-        global.fetch = jest.fn(() =>
-            Promise.resolve({
-                ok: true,
-                blob: () => Promise.resolve({
-                    type: 'image/jpeg',
+    describe('should return a resolved promise', () => {
+        it('if response is ok, content is binary and image/jpeg', async () => {
+            // Arrange
+            // https://www.leighhalliday.com/mock-fetch-jest
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                    blob: () => Promise.resolve({
+                        type: 'image/jpeg',
+                    })
                 })
-            })
-        );
+            );
+    
+            // https://stackoverflow.com/a/66978762
+            let onloadRef;
+            const sampleResponse = 'data:image/jpeg;base64,...';
+            global.FileReader = jest.fn(() => {
+                const fileReader = {
+                    readAsDataURL: jest.fn(() => onloadRef({
+                        target: {
+                            result: sampleResponse
+                        }
+                    }))
+                };
+                Object.defineProperty(fileReader, 'onload', {
+                    get() {
+                      return this._onload;
+                    },
+                    set(onload) {
+                      onloadRef = onload;
+                      this._onload = onload;
+                    },
+                  });
+    
+                return fileReader;
+            });
+    
+            // Act
+            // Assert
+            await expect(downloadAndEncodeToBase64('https://127.0.0.1/')).resolves.toBe(sampleResponse);
+        });
+    });
 
-        // https://stackoverflow.com/a/66978762
-        let onloadRef;
-        const sampleResponse = 'data:image/jpeg;base64,...';
-        global.FileReader = jest.fn(() => {
-            const fileReader = {
-                readAsDataURL: jest.fn(() =>  onloadRef({
-                    target: {
-                        result: sampleResponse
-                    }
-                }))
-            };
-            Object.defineProperty(fileReader, 'onload', {
-                get() {
-                  return this._onload;
-                },
-                set(onload) {
-                  onloadRef = onload;
-                  this._onload = onload;
-                },
-              });
-
-            return fileReader;
+    describe('should return a rejected promise', () => {
+        it('if response is not ok', async () => {
+            // Arrange
+            // https://www.leighhalliday.com/mock-fetch-jest
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: false
+                })
+            );
+    
+            // Act
+            // Assert
+            await expect(downloadAndEncodeToBase64('https://127.0.0.1/')).rejects.toBeUndefined();
         });
 
-        // Act
-        return downloadAndEncodeToBase64('https://127.0.0.1/').then(response => {
+        it('if response is ok, but content is not binary', async () => {
+            // Arrange
+            // https://www.leighhalliday.com/mock-fetch-jest
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true
+                })
+            );
+    
+            // Act
             // Assert
-            expect(response).toBe(sampleResponse);
+            await expect(downloadAndEncodeToBase64('https://127.0.0.1/')).rejects.toBeUndefined();
+        });
+
+        it('if response is ok, content is binary, but not image/jpeg', async () => {
+            // Arrange
+            // https://www.leighhalliday.com/mock-fetch-jest
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                    blob: () => Promise.resolve({
+                        type: 'application/json',
+                    })
+                })
+            );
+    
+            // Act
+            // Assert
+            await expect(downloadAndEncodeToBase64('https://127.0.0.1/')).rejects.toBeUndefined();
         });
     });
 });
