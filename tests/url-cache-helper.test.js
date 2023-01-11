04568b86d5e3d3700d109c3c065dea9c987c9aa8
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-01-11 01:25:34 +0100

    Added tests for the urlCacheHelper: added tests for the case when cache is empty

--- tests/url-cache-helper.test.js
+++ tests/url-cache-helper.test.js
@@ -4,42 +4,70 @@ import { imageCacheResults } from './testData';
 const urlCacheName = 'urlCache';
 
 describe('urlCacheHelper.get', () => {
-    const mockGetItem = jest.fn().mockReturnValue(JSON.stringify(Object.keys(imageCacheResults).map(query => ({
-        query,
-        urls: imageCacheResults[query],
-        date: new Date()
-    }))));
-
-    global.localStorage = {
-        getItem: mockGetItem,
-        setItem: jest.fn(),
-    };
-
-    describe('should return a resolved promise', () => {
-        Object.keys(imageCacheResults).forEach(key => {
-            it(`if ${key} exists in cache`, () => {
+    describe('cache is not empty', () => {
+        const mockGetItem = jest.fn().mockReturnValue(JSON.stringify(Object.keys(imageCacheResults).map(query => ({
+            query,
+            urls: imageCacheResults[query],
+            date: new Date()
+        }))));
+    
+        global.localStorage = {
+            getItem: mockGetItem,
+            setItem: jest.fn(),
+        };
+    
+        describe('should return a resolved promise', () => {
+            Object.keys(imageCacheResults).forEach(key => {
+                it(`if ${key} exists in cache`, () => {
+                    // Arrange
+    
+                    // Act
+                    const result = urlCacheHelper.get(key);
+    
+                    // Assert
+                    expect(result).resolves.toEqual(imageCacheResults[key]);
+                    expect(mockGetItem).toHaveBeenNthCalledWith(1, urlCacheName);
+                    mockGetItem.mockClear();
+                });
+            });
+        });
+    
+        describe('should return a rejected promise', () => {
+            it('if a country does not exist in cache', () => { 
                 // Arrange
-
-                // Act
-                const result = urlCacheHelper.get(key);
-
+    
+                // Act  
+                const result = urlCacheHelper.get('test');
+    
                 // Assert
-                expect(result).resolves.toEqual(imageCacheResults[key]);
+                expect(result).rejects.toBeUndefined();
                 expect(mockGetItem).toHaveBeenNthCalledWith(1, urlCacheName);
+                mockGetItem.mockClear();
             });
         });
     });
 
-    describe('should return a rejected promise', () => {
-        it('if a country does not exist in cache', () => { 
-            // Arrange
-
-            // Act  
-            const result = urlCacheHelper.get('test');
-
-            // Assert
-            expect(result).rejects.toBeUndefined();
-            expect(mockGetItem).toHaveBeenNthCalledWith(1, urlCacheName);
+    describe('cache is empty', () => {
+        describe('should return a rejected promise', () => {
+            [JSON.stringify([]), ''].forEach(value => {
+                it('if urlCache is an empty array', () => { 
+                    // Arrange
+                    const mockGetItem = jest.fn().mockReturnValue(value);
+        
+                    global.localStorage = {
+                        getItem: mockGetItem,
+                        setItem: jest.fn(),
+                    };
+    
+                    // Act  
+                    const result = urlCacheHelper.get('argentina');
+        
+                    // Assert
+                    expect(result).rejects.toBeUndefined();
+                    expect(mockGetItem).toHaveBeenNthCalledWith(1, urlCacheName);
+                    mockGetItem.mockClear();
+                });
+            });
         });
     });
 });
